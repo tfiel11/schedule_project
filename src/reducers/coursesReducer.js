32d49d8012dce60cd89627f1141c81a9673250fb
Tyler Fielding <tfiel11@Tylers-MacBook-Pro.local>
2018-07-06 14:15:51 -0600

    Updated app up until the styling

--- src/reducers/coursesReducer.js
+++ src/reducers/coursesReducer.js
@@ -1,4 +1,7 @@
-import { FETCH_COURSES } from '../actions/types';
+import { 
+    FETCH_COURSES,
+    TOGGLE_ENROLLED
+} from '../actions/types';
 
 export default function(state = [], action) {
     switch (action.type) {
@@ -6,6 +9,17 @@ export default function(state = [], action) {
             return [
                 ...state,
                 ...action.payload
+            ]
+        case TOGGLE_ENROLLED:
+            console.log(action.payload)
+            const newCourses = state.map(course => {
+                if(course.id == action.payload) {
+                    course.enrolled = !course.enrolled
+                }
+                return course
+            })
+            return [
+                ...newCourses
             ]
         default:
             return state;
